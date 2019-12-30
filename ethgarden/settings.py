293043bd628c23d4e27da95a5080c188c0b6e633
s154674 <s154674@student.dtu.dk>
2019-12-30 15:17:02 +0100

    django cors + requirements clean

--- ethgarden/settings.py
+++ ethgarden/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'customauth',
     'events',
     'plants',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ethgarden.urls'
@@ -144,6 +146,12 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+       'https://localhost:3000',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
