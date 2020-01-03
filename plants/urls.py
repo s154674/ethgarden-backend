fe9e2f5df6f24f5cd27b277da9e91d322ec99de3
s154674 <s154674@student.dtu.dk>
2020-01-03 15:11:29 +0100

    individual plant views

--- plants/urls.py
+++ plants/urls.py
@@ -3,4 +3,5 @@ from plants import views
 
 urlpatterns = [
     path('plants/<str:owner>/', views.PlantList.as_view()),
+    path('plant/<int:pk>/', views.PlantDetail.as_view()),
 ]
--- plants/views.py
+++ plants/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from customauth.custom_permission import OwnerOrReadOnly
+from customauth.custom_permission import OwnerOrReadOnly, OwnerOrReadOnly2
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 # from rest_framework_simplejwt.views import TokenViewBase
@@ -11,7 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
-class PlantList(generics.ListAPIView, APIView):
+class PlantList(generics.ListAPIView):
     permission_classes = (OwnerOrReadOnly,)
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
@@ -20,3 +20,14 @@ class PlantList(generics.ListAPIView, APIView):
     def get_queryset(self):
         owner = self.kwargs['owner']
         return Plant.objects.filter(owner=owner)
+
+class PlantDetail(generics.RetrieveAPIView):
+    permission_classes = (OwnerOrReadOnly2,)
+    queryset = Plant.objects.all()
+    serializer_class = PlantSerializer
+
+    # def get_queryset(self):
+    #     plant_id = self.kwargs['plant_id']
+    #     print(plant_id)
+    #     return Plant.objects.get(plant_id=plant_id)
+    
